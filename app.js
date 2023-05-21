0b367795246703a5aca93c165a33a580a80d80c3
MD Faizan Alam <mac020@nist.edu>
2023-05-21 13:45:26 +0530

    updated package.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 80 || process.env.PORT;
+const port = process.env.PORT || 80;
 
 app.use(cors()); //put this line before other app.use always!!
 
